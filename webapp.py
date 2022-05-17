6d482baf1819f27ee723775d738b36026cd23032
Ronnie <ronnie1999@naver.com>
2022-05-18 00:04:18 +0900

    save detected file cropped

--- webapp.py
+++ webapp.py
@@ -25,8 +25,9 @@ def predict():
         img_bytes = file.read()
         img = Image.open(io.BytesIO(img_bytes))
         results = model(img, size=640)
+        crops=results.crop(save=True)
 
-        # for debugging
+        # #for debugging
         # data = results.pandas().xyxy[0].to_json(orient="records")
         # return data
 
@@ -44,8 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load(
-        "ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True, autoshape=True
-    )  # force_reload = recache latest code
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/best.pt')
+    # model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True, autoshape=True)  # force_reload = recache latest code
     model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
